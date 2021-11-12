fc47351258f8ffac710a13971521d950fea6112c
Yucheng Jin <yucheng.jin.sr@gmail.com>
2021-11-12 23:15:38 +0800

    docfix

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import { MuzzleFlashDemo } from './muzzleFlashDemo.js';
+import { ShipTrail } from './shipTrail';
 
 
 let scene, camera, renderer, clock;
 let demo;
+let shipTrail;
+let mesh;
+
 
 function init() {
 
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
-    camera.position.z = 10;
+    camera.position.z = 50;
 
     scene = new THREE.Scene();
 
@@ -31,15 +35,20 @@ function init() {
 
 function setupScene() {
 
-	var mesh = new THREE.Mesh(
+	mesh = new THREE.Mesh(
 		new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 0xff0000 })
 	);
 
-    demo = new MuzzleFlashDemo();
+    scene.add(mesh);
 
-    scene = demo.initScene();
+    // demo = new MuzzleFlashDemo();
 
-	scene.add(mesh);
+    // scene = demo.initScene();
+
+    shipTrail = new ShipTrail();
+
+	scene.add(shipTrail);
+    scene.add(shipTrail.batchRenderer);
 
 }
 
@@ -49,9 +58,18 @@ function animate() {
 
     var delta = clock.getDelta();
 
-    demo.render(delta);
+    // demo.render(delta);
+
+    shipTrail.update(delta);
+
+    shipTrail.position.z -= 0.5;
+    shipTrail.position.z =  shipTrail.position.z % 50;
+    mesh.position.z = shipTrail.position.z;
+    // shipTrail.position.y += Math.random() - 0.5;
 
     renderer.render(scene, camera);
+
+    console.log(">>> calls: ", renderer.info.render.calls);
 
 }
 
